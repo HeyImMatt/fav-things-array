90dc4300d66eb056dbb6041e569c55bd9ad8ceb0
heyimmatt <mrmcfarland@gmail.com>
2020-06-08 10:37:12 -0700

    Add a modifiedArray variable and output original answers to a ul item on the webpage
    Co-authored-by: Megan Hepner <megan.hepner@gmail.com>

--- scripts.js
+++ scripts.js
@@ -18,8 +18,16 @@ $(document).ready(function() {
     const answer4 = $('#fav-thing4').val();
     const answer5 = $('#fav-thing5').val();
     const originalArray = [];
+    const modifiedArray = [];
 
     originalArray.push(answer1, answer2, answer3, answer4, answer5);
 
+    modifiedArray.push(originalArray[1], originalArray[0], originalArray[2])
+
+    $('#original-answers').append(`<li>${originalArray[0]}</li>`)
+    $('#original-answers').append(`<li>${originalArray[1]}</li>`)
+    $('#original-answers').append(`<li>${originalArray[2]}</li>`)
+    $('#original-answers').append(`<li>${originalArray[3]}</li>`)
+    $('#original-answers').append(`<li>${originalArray[4]}</li>`)
   })
 })
