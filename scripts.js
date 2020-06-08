60e7fe3e0900f166328311118736d6b1acd746b4
heyimmatt <mrmcfarland@gmail.com>
2020-06-08 10:11:55 -0700

    Add first survey question
    Co-authored-by: Megan Hepner <megan.hepner@gmail.com>

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//Need a form with text fields on the webpage
+
+//Write js/jquery code to gather the input of the form fields
+
+// put the form input into a sinlge array
+
+// use square bracket notation to access second, first and third elements of the original array
+// and use push to add these elements to a new array
+
+//display new array in your page as list items in a ul tag
