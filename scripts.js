d6ff01ce599d9fa4cadbc4529110e44161853f1f
heyimmatt <mrmcfarland@gmail.com>
2020-06-08 10:29:57 -0700

    Add jQuery to get the form input, store the answers in variables, and push the answers to an array
    Co-authored-by: Megan Hepner <megan.hepner@gmail.com>

--- scripts.js
+++ scripts.js
@@ -8,3 +8,18 @@
 // and use push to add these elements to a new array
 
 //display new array in your page as list items in a ul tag
+
+$(document).ready(function() {
+  $('form#fav-things').submit(function(event) {
+    event.preventDefault();
+    const answer1 = $('#fav-thing1').val();
+    const answer2 = $('#fav-thing2').val();
+    const answer3 = $('#fav-thing3').val();
+    const answer4 = $('#fav-thing4').val();
+    const answer5 = $('#fav-thing5').val();
+    const originalArray = [];
+
+    originalArray.push(answer1, answer2, answer3, answer4, answer5);
+
+  })
+})
